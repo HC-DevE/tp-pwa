fc7f909c3a0604ba864c2384411e7d5336de08e5
darkyHC <108454489+darkyHC@users.noreply.github.com>
2024-05-01 17:33:51 +0200

    fix header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="default" class="h-screen">
 
-      <AppHeader class="w-full flex items-center justify-around px-2 h-10 my-2 bg-chathams-blue-800 " />
+      <AppHeader class="w-full flex items-center justify-around px-2 h-10 bg-chathams-blue-800 dark:text-white " />
       
       <div class="flex w-full mx-auto h-screen">
         <slot />
       </div>
 
-      <AppFooter class=" z-50 bottom-0 flex w-full h-[70px] border-t border-t-gray-700 bg-chathams-blue-800 " />
+      <AppFooter class="z-50 bottom-0 flex w-full h-[70px] border-t border-t-gray-700 bg-chathams-blue-800 " />
       <!-- <div class=" fixed inset-0 bg-gradient-to-tr from-emerald-500 to-cyan-200 z-[-1] opacity-50" /> -->
     </div>
   </template>
