0dc153b62114b2333648a43ea1ff321ddca911f2
darkyHC <108454489+darkyHC@users.noreply.github.com>
2024-05-01 19:10:55 +0200

    fix otp verifier

--- pages/otp-verification.vue
+++ pages/otp-verification.vue
@@ -22,25 +22,24 @@ const otpCode = ref('');
 //     }
 // }
 
-// Dans votre composant Vue ou fichier JavaScript
 async function receiveOTP() {
-    if ('OTPCredential' in window) {
-        try {
-            const content = await navigator.credentials.get({
-                otp: { transport:['sms'] },
-                signal: abortController.signal // Assurez-vous d'avoir un AbortController si nécessaire
-            });
-            console.log(content.code); // Vous devriez voir le code OTP ici
-            alert('Received OTP: ' + content.code); // Affichez le code OTP (à des fins de démonstration uniquement
-            otpCode.value = content.code;
-            navigator.vibrate([200, 100, 200]); // Vibrate if the code is received
-            return content.code;
-        } catch (error) {
-            console.error('OTP Retrieval failed:', error);
-        }
-    } else {
-        console.log('WebOTP API not supported on this browser.');
+    // if ('OTPCredential' in window) {
+    try {
+        const content = await navigator.credentials.get({
+            otp: { transport: ['sms'] },
+            signal: abortController.signal // Assurez-vous d'avoir un AbortController si nécessaire
+        });
+        alert('Received OTP: ' + content.code); // Affichez le code OTP (à des fins de démonstration uniquement
+        otpCode.value = content.code;
+        navigator.vibrate([200, 100, 200]); // Vibrate if the code is received
+        if(!!content.code) window.navigator.vibrate(500);
+        return content.code;
+    } catch (error) {
+        console.error('OTP Retrieval failed:', error);
     }
+    // } else {
+    //     console.log('WebOTP API not supported on this browser.');
+    // }
 }
 
 </script>
